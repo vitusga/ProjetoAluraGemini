e980033d2c9eb9710eeec84b6268cf65e48b0662
Vítor <68357527+vitusga@users.noreply.github.com>
2024-09-05 10:33:31 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let bebidas = [
+    {
+        título: "Espresso",
+        descrição: "O espresso,por sua extração sob pressão, é a alma de qualquer cafeteria especializada. Seu corpo robusto e complexidade de sabores e aromas proporcionam uma experiência sensorial única, capaz de despertar os sentidos dos amantes de café mais exigentes.",
+        Preparo: "Pode variar de 18g a 22g no porta-filtro duplo. É servido na xícara podendo conter de 20g a 60g de bebida final.",
+        link: "https://revistaespresso.com.br/quais-os-caminhos-do-barista/"
+    },
+    {
+        título: "Cappuccino",
+        descrição: "O cappuccino, uma bebida originária da Itália, é um clássico apreciado por amantes de café ao redor do mundo. Essa bebida quentinha e cremosa é preparada com café espresso, leite vaporizado e crema de leite, formando camadas distintas em uma xícara. O resultado é uma experiência sensorial única, que equilibra a força do café com a suavidade do leite e a doçura da crema.",
+        Preparo: "Uma dose de espresso, acrescida de leite vaporizado, servido em xícara de 200ml",
+        link: "https://www.instagram.com/livraria.telaranha/"
+    },
+    {
+        título: "Latte",
+        descrição: "O latte é uma bebida de café italiana que conquistou o paladar de pessoas ao redor do mundo por sua combinação harmoniosa de sabores e texturas. É uma opção popular para quem busca um café mais suave e cremoso, em comparação com o cappuccino.",
+        Preparo: "Um dose de espresso, acrescida de leite vaporizado, servido na xícara de 400ml.",
+        Link: "oi"
+    },
+    {
+        título: "Cold Brew",
+        descrição: "Cold Brew. É uma bebida que tem conquistado cada vez mais adeptos por sua complexidade e sabor único. Preparado por meio de uma infusão lenta e prolongada em água fria, o cold brew revela uma bebida de perfil intenso e de acidez equilibrada.",
+        link: "https://revistaespresso.com.br/quais-os-caminhos-do-barista/",
+        Preparo: "ois"
+    }
+];
+
